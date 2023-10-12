09f2230c05b808b69b427e61329bdfb65808a80e
kwiecs01 <szymon.kwiecinski@stepstone.com>
2023-10-12 21:05:10 +0200

    done

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 import uuid
 
 from flask import (
@@ -23,15 +24,30 @@ pages = Blueprint(
 )
 
 
+def login_required(route):
+    @functools.wraps(route)
+    def route_wrapper(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect(url_for(".login"))
+
+        return route(*args, **kwargs)
+
+    return route_wrapper
+
+
 @pages.route("/")
+@login_required
 def index():
-    movie_data = current_app.db.movie.find({})
+    user_data = current_app.db.user.find_one({"email": session["email"]})
+    user = User(**user_data)
+    movie_data = current_app.db.movie.find({"_id": {"$in": user.movies}})
 
     movies = [Movie(**movie) for movie in movie_data]
     return render_template("index.html", title="Movie Watchlist", movies_data=movies)
 
 
 @pages.route("/add", methods=["GET", "POST"])
+@login_required
 def add_movie():
     form = MovieForm()
 
@@ -44,6 +60,9 @@ def add_movie():
         )
 
         current_app.db.movie.insert_one(asdict(movie))
+        current_app.db.user.update_one(
+            {"_id": session["user_id"]}, {"$push": {"movies": movie._id}}
+        )
 
         return redirect(url_for(".movie", _id=movie._id))
 
@@ -53,6 +72,7 @@ def add_movie():
 
 
 @pages.route("/edit/<string:_id>", methods=["GET", "POST"])
+@login_required
 def edit_movie(_id: str):
     movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
     form = ExtendedMovieForm(obj=movie)
@@ -81,6 +101,7 @@ def movie(_id: str) -> None:
 
 
 @pages.get("/movie/<string:_id>/rate")
+@login_required
 def rate_movie(_id: str):
     rating = int(request.args.get("rating"))
     current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
@@ -89,6 +110,7 @@ def rate_movie(_id: str):
 
 
 @pages.get("/movie/<string:_id>/watch")
+@login_required
 def watch_today(_id):
     current_app.db.movie.update_one(
         {"_id": _id}, {"$set": {"last_watched": datetime.datetime.today()}}
@@ -146,6 +168,14 @@ def login():
 
     return render_template("login.html", title="Movies Watchlist - Login", form=form)
 
+
+@pages.route("/logout")
+def logout():
+    current_theme = session.get("theme")
+    session.clear()
+    session["theme"] = current_theme
+
+    return redirect(url_for(".login"))
 
 @pages.get("/toggle-theme")
 def toggle_theme():
