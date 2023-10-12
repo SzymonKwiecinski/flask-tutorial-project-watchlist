60c1fedd4a1a062b8605a148b2b76505240e4f00
kwiecs01 <szymon.kwiecinski@stepstone.com>
2023-10-12 20:26:56 +0200

    Add user signups

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -10,11 +10,13 @@ from flask import (
     current_app,
     url_for,
     abort,
+    flash
 )
 
-from movie_library.models import Movie
-from movie_library.forms import MovieForm, ExtendedMovieForm
+from movie_library.models import Movie, User
+from movie_library.forms import MovieForm, ExtendedMovieForm, RegisterForm
 from dataclasses import asdict
+from passlib.hash import pbkdf2_sha256
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -49,6 +51,7 @@ def add_movie():
         "new_movie.html", title="Movies Watchlist - Add Movie", form=form
     )
 
+
 @pages.route("/edit/<string:_id>", methods=["GET", "POST"])
 def edit_movie(_id: str):
     movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
@@ -66,7 +69,6 @@ def edit_movie(_id: str):
         current_app.db.movie.update_one({"_id": movie._id}, {"$set": asdict(movie)})
         return redirect(url_for(".movie", _id=movie._id))
     return render_template("movie_form.html", movie=movie, form=form)
-
 
 
 @pages.get("/movie/<string:_id>")
@@ -94,6 +96,30 @@ def watch_today(_id):
 
     return redirect(url_for(".movie", _id=_id))
 
+
+@pages.route("/register", methods=["GET", "POST"])
+def register():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(
+            _id=uuid.uuid4().hex,
+            email=form.email.data,
+            password=pbkdf2_sha256.hash(form.password.data)
+        )
+
+        current_app.db.user.insert_one(asdict(user))
+
+        flash("User registered successfully", "success")
+
+        return redirect(url_for(".index"))
+
+    return render_template(
+        "register.html", title="Movies Watchlist - Register", form=form
+    )
 
 @pages.get("/toggle-theme")
 def toggle_theme():
